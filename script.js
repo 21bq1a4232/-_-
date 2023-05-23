99a3d3085ceccda82a68cb499ac3aabe194214e3
Danda Pranav Krishna <113225675+21bq1a4232@users.noreply.github.com>
2023-05-23 16:12:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+const inputBox = document.getElementById("inputbox");
+const list1 = document.getElementById("list-content");
+
+function addTask(){
+    if(inputBox.value === ''){
+        alert("You must write something!!!");
+    }
+    else{
+        let li =document.createElement("li");
+        li.innerHTML = inputBox.value;
+        list1.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = "";
+    saveData();
+}
+
+list1.addEventListener("click", function(e){
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+},false);
+
+function saveData(){
+    localStorage.setItem("data", list1.innerHTML);
+}
+function showTask() {
+    list1.innerHTML = localStorage.getItem("data");
+}
+showTask();
